950cab4405476f06f9cba39e965c1a3ac0669407
peiyuchen <crazypei23@gmail.com>
2015-10-30 03:35:49 +0800

    Improve findName algorithm by hash function and smaller struct. - Try to use two arrays of linked list to save all lastName struct. - Use hash2, in the textbook,to create index for each entry. - Deal by smaller struct. - Special thanks:Mimihalo. - Reference: https://github.com/mimihalo/phonebook

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,15 +1,52 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "phonebook_opt.h"
 
-/* FILL YOUR OWN IMPLEMENTATION HERE! */
-entry *findName(char lastname[], entry *pHead)
+/* original version */
+
+entry* hash_table[hash_size];
+entry* hash_tail[hash_size];
+entry *findName(char lastName[], entry *pHead)
 {
-    /* TODO: implement */
+    unsigned int index = hash2(lastName);
+    entry* tmp = hash_table[index];
+    while (tmp != NULL) {
+        if (strcasecmp(lastName, tmp->lastName) == 0)
+            return tmp;
+	
+        tmp = tmp->pNext;
+    }
     return NULL;
 }
 
+
 entry *append(char lastName[], entry *e)
 {
-    return NULL;
+    /* allocate memory for the new entry and put lastName */
+    entry* t = (entry *) malloc(sizeof(entry));
+    strcpy(t->lastName, lastName);
+    t->pNext = NULL;
+
+    unsigned int index = hash2(lastName);
+    if(hash_table[index] == NULL){
+	hash_table[index] = t;
+    }else{
+	//printf("FUCK it: %s\n", pHead->lastName);
+	hash_tail[index]->pNext = t;
+    }
+    hash_tail[index] = t;
+
+    return e;
+}
+unsigned int hash2(char *key)
+{
+    unsigned int hashVal = 0;
+    while(*key != '\0'){
+        hashVal = (hashVal << 5) + *key++;
+    }
+
+    return hashVal % hash_size;
 }
--- phonebook_opt.h
+++ phonebook_opt.h
@@ -2,10 +2,10 @@
 #define _PHONEBOOK_H
 
 #define MAX_LAST_NAME_SIZE 16
+#define hash_size 42737
 
 /* original version */
 typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
     char firstName[16];
     char email[16];
     char phone[10];
@@ -16,9 +16,15 @@ typedef struct __PHONE_BOOK_ENTRY {
     char state[2];
     char zip[5];
     struct __PHONE_BOOK_ENTRY *pNext;
+} lastNameEntry;
+
+typedef struct __LAST_NAME_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    lastNameEntry *detail;
+    struct __LAST_NAME_ENTRY *pNext;
 } entry;
 
-entry *findName(char lastname[], entry *pHead);
+entry *findName(char lastName[], entry *pHead);
 entry *append(char lastName[], entry *e);
-
+unsigned int hash2(char *key);
 #endif
